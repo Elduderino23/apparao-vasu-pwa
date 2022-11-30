d19299b099333cd0db25426c8502720a85f704b4
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-11-29 21:49:19 -0800

    askBCS Troubleshoot 3

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,45 +13,48 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) =>
-console.log('Post to the database');
+export const putDb = async (content) => {
+  console.log('Post to the database');
 
 
-const contactDb = await openDB('contact', 1);
+  const contactDb = await openDB('contact', 1);
 
 
-const tx = contactDb.transaction('contact', 'readwrite');
+  const tx = contactDb.transaction('contact', 'readwrite');
 
 
-const store = tx.objectStore('contact');
+  const store = tx.objectStore('contact');
 
 
-const request = store.add({ });
+  const request = store.add({ });
 
 
-const result = await request;
-console.log('🚀 - data saved to the database', result);
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+  };
+
+
 
 
 export const getDb = async () => {
   console.log('GET from the database');
 
 
-const contactDb = await openDB('contact', 1);
+  const contactDb = await openDB('contact', 1);
 
 
-const tx = contactDb.transaction('contact', 'readonly');
+  const tx = contactDb.transaction('contact', 'readonly');
 
 
-const store = tx.objectStore('contact');
+  const store = tx.objectStore('contact');
 
 
-const request = store.getAll();
+  const request = store.getAll();
 
-
-const result = await request;
-console.log('result.value', result);
-return result;
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
+
 
 initdb();
