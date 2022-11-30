604cb4ed6b12b518be858f5022b1612069805534
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-11-29 18:25:38 -0800

    add injectManifest fro webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -24,6 +24,28 @@ module.exports = () => {
           title: 'Webpack Plugin',
         }),
         new MiniCssExtractPlugin(),
+        new WebpackPwaManifest({
+          fingerprints: false,
+          inject: true,
+          name: 'Contact Cards',
+          short_name: 'Contact',
+          description: 'Never forget your contacts!',
+          background_color: '#225ca3',
+          theme_color: '#225ca3',
+          start_url: './',
+          publicPath: './',
+          icons: [
+            {
+              src: path.resolve('src/images/logo.png'),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join('assets', 'icons'),
+            },
+          ],
+        }),
+        new InjectManifest({
+          swSrc: './src-sw.js',
+          swDest: 'src-sw.js',
+        }),
     ],
 
     module: {
