8cc865484596e07b65b60d9fbdf03ff21e6fdc5d
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-11-29 19:09:10 -0800

    make changes to src-sw.js and webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -54,6 +54,18 @@ module.exports = () => {
             test: /\.css$/i,
             use: [MiniCssExtractPlugin.loader, 'css-loader'],
           },
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            // We use babel-loader in order to use ES6.
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              },
+            },
+          },
       ],
     },
   };
