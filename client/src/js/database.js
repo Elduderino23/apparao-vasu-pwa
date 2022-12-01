3836f139dc5476d8c3e8b986fa6daa3bc31bca03
Elduderino23 <vasutamirisaapparao@gmail.com>
2022-11-30 19:34:59 -0800

    fix DOMExection: Failed to execute transaction on ‘IDBDatabase’ errors cause by code in database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,16 +17,16 @@ export const putDb = async (content) => {
   console.log('Post to the database');
 
 
-  const contactDb = await openDB('contact', 1);
+  const contactDb = await openDB('jate', 1);
 
 
-  const tx = contactDb.transaction('contact', 'readwrite');
+  const tx = contactDb.transaction('jate', 'readwrite');
 
 
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
 
-  const request = store.add({ });
+  const request = store.put({ id: 1, value: content });
 
 
   const result = await request;
@@ -40,20 +40,20 @@ export const getDb = async () => {
   console.log('GET from the database');
 
 
-  const contactDb = await openDB('contact', 1);
+  const contactDb = await openDB('jate', 1);
 
 
-  const tx = contactDb.transaction('contact', 'readonly');
+  const tx = contactDb.transaction('jate', 'readonly');
 
 
-  const store = tx.objectStore('contact');
+  const store = tx.objectStore('jate');
 
 
-  const request = store.getAll();
+  const request = store.get(1);
 
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result?.value;
 };
 
 
